5707b1ef676ab2b03c57797f33ef8a1ca46c3254
nalinbansl-s <nalinbansal.s@gmail.com>
2022-10-05 15:15:20 +0530

    new

--- src/test/java/org/samp/Javass.java
+++ src/test/java/org/samp/Javass.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
 	System.out.println("sample 1");
 	System.out.println("sample 2");
 	System.out.println("sample 3");
-	System.out.println("sample 4");
+	
 }
 }
