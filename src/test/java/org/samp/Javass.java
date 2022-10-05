464898d23569b4443fbdb8f535364952678981e3
nalinbansl-s <nalinbansal.s@gmail.com>
2022-10-05 14:48:44 +0530

    new code added

---
+++ src/test/java/org/samp/Javass.java
@@ -0,0 +1,9 @@
+package org.samp;
+
+public class Javass {
+public static void main(String[] args) {
+	System.out.println("sample 1");
+	System.out.println("sample 2");
+	System.out.println("sample 3");
+}
+}
